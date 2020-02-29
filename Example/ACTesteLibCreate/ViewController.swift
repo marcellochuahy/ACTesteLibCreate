a186fe839938b271d8ccf7a649cd4068ca2d6146
marcellochuahy <marcello@chuahy.org>
2020-02-29 16:03:26 -0300

    Add Source files

--- Example/ACTesteLibCreate/ViewController.swift
+++ Example/ACTesteLibCreate/ViewController.swift
@@ -7,18 +7,34 @@
 //
 
 import UIKit
+import ACTesteLibCreate // DScrollView
+import TinyConstraints
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  
+  let scrollView = DScrollView()
+  let scrollViewContainer = DScrollViewContainer(axis: .vertical, spacing: 10)
+  let scrollViewElement0 = DScrollViewElement(height: 1200, backgroundColor: .purple)
+  let scrollViewElement1 = DScrollViewElement(height: 500, backgroundColor: .purple)//.withBackground(image: #imageLiteral(resourceName: "icon.png"), contentMode: .scaleAspectFit)
+  
+  let label = UILabel()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .white
+    label.text = "I am a label"
+    
+    view.addScrollView(scrollView,
+                       withSafeArea: .none,
+                       hasStatusBarCover: true,
+                       statusBarBackgroundColor: .white,
+                       container: scrollViewContainer,
+                       elements: scrollViewElement0, scrollViewElement1)
+    
+    scrollViewElement1.addSubview(label)
+    label.centerInSuperview()
+  }
+  
 }
 
